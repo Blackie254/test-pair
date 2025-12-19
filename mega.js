3a85538a81df04053f1d7499f8fbe68247c79df1
Blackie254 <manoastephen14@gmail.com>
2025-12-19 11:26:16 +0300

    Change Mega password in mega.js
    Updated Mega authentication password.

--- mega.js
+++ mega.js
@@ -3,7 +3,7 @@ import * as mega from 'megajs';
 // Mega authentication credentials
 const auth = {
     email: 'stephenmanoa5@gmail.com', // Replace with your Mega email
-    password: '27W5vF1649', // Replace with your Mega password
+    password: 'jyS89S2025@#@.', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
